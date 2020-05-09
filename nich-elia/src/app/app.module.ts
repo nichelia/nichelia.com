49b1a12155f9806ddf933cf8bad10aa40c5c74df
nichelia <nich.elia@gmail.com>
2020-05-09 19:35:14 +0100

    Added Angular Material

--- nich-elia/src/app/app.module.ts
+++ nich-elia/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { NeLogoComponent } from './ne-logo/ne-logo.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { NeLogoComponent } from './ne-logo/ne-logo.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
